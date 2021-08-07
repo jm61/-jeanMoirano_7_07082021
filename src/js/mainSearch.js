d589a8a99c9fb2e887dd43f65959ac0339235681
jm61 <jean.moirano@gmail.com>
2021-08-07 22:56:56 +0200

    complete main search

--- src/js/mainSearch.js
+++ src/js/mainSearch.js
@@ -12,26 +12,71 @@ const recipesWrapper = _.$('#recipes-wrapper')
             hiddenCont.textContent = ''
             let searchResult = []
 
-            if(e.target.value.length < 3) return
+            if(e.target.value.length < 3) {
+                new DisplayRecipes(recipes)
+            }
             const rand = Math.random().toFixed(4)
             console.time(rand)
             recipes.forEach(recipe => {
-                if(_.flatText(recipe.name).includes(e.target.value)) {
+
+                // search on list of ingredients
+                getIngredients(recipe).forEach(el => {
+                    if(_.flatText(el).includes(e.target.value)) {
+                        searchResult.push(recipe)
+                        console.log(el)
+                    }
+                })
+                // search on list of ustensils
+                getUstensils(recipe).forEach(el => {
+                    if(_.flatText(el).includes(e.target.value)) {
+                        searchResult.push(recipe)
+                        console.log(el)
+                    }
+                })                  
+                // search on name, description and appliance
+                if(
+                    _.flatText(recipe.name).includes(e.target.value) ||
+                    _.flatText(recipe.description).includes(e.target.value) ||
+                    _.flatText(recipe.appliance).includes(e.target.value)
+                        ) {
                     searchResult.push(recipe)
                 }  
             })          
             new DisplayRecipes(searchResult)
             console.timeEnd(rand) 
             if(recipesWrapper.innerHTML.length === 0) {
-                invalidSearch(e.target.value)
+                invalidSearch(e.target.value)  
             }
         })   
     }
- 
-
 
 const invalidSearch = value => {
     hiddenCont.textContent = `votre recherche avec "${value}" ne donne aucun résultat`
+}
+/**
+ * @param recipe
+ * @returns listUstensils of one recipe
+ */
+function getUstensils(recipe){
+    let listUstensils = []
+    const {id,name,servings,time,description,appliance,ingredients,...getUstensils} = recipe
+    getUstensils.ustensils.forEach(el => {
+        listUstensils.push(el)   
+    })
+    return listUstensils
+}
+
+/**
+ * @param recipe
+ * @returns listIngredientq of one recipe
+ */
+function getIngredients(recipe){
+    let listIngredients = []
+    const {id,name,servings,time,description,appliance,ustensils,...getIngredients} = recipe
+    getIngredients.ingredients.forEach(el => {
+        listIngredients.push(el.ingredient)   
+    })
+        return listIngredients
 }
 
 
