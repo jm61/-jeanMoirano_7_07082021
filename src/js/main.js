8d970bf2f7b8d353ab1abb75a76644f993dc9e89
jm61 <jean.moirano@gmail.com>
2021-08-09 10:07:55 +0200

    main search and dropdown fixed

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,11 @@
 import {recipes} from './data.js'
-import {dropDownMenus} from './dropDown.js';
+import {dropDownMenus} from './dropDown.js'
 import {DisplayRecipes} from './recipes.js'
+import {search} from './mainSearch.js'
 
-new dropDownMenus()
+new dropDownMenus(recipes)
 new DisplayRecipes(recipes)
+search()
 
 
 
--- src/js/mainSearch.js
+++ src/js/mainSearch.js
@@ -1,83 +1,100 @@
-import {recipes} from './data.js'
-import {DisplayRecipes} from './recipes.js'
-import * as _ from './utils.js'
-const mainSearch = _.$('#search')
-const hiddenCont = _.$('.search-error-msg')
-const recipesWrapper = _.$('#recipes-wrapper')
+import { recipes } from "./data.js"
+import { DisplayRecipes } from "./recipes.js"
+import { dropDownMenus } from "./dropDown.js"
+import * as _ from "./utils.js"
+const mainSearch = _.$("#search")
+const hiddenCont = _.$(".search-error-msg")
+const recipesWrapper = _.$("#recipes-wrapper")
 
-   export function search () {
-        mainSearch.value = ''
-        mainSearch.addEventListener('input', e => {
-            recipesWrapper.innerHTML = ''
-            hiddenCont.textContent = ''
-            let searchResult = []
+function search() {
+  mainSearch.value = ""
+  mainSearch.addEventListener("input", (e) => {
+    let searchResult = []
+    recipesWrapper.innerHTML = ""
+    hiddenCont.textContent = ""
 
-            if(e.target.value.length < 3) {
-                new DisplayRecipes(recipes)
-            }
-            const rand = Math.random().toFixed(4)
-            console.time(rand)
-            recipes.forEach(recipe => {
-
-                // search on list of ingredients
-                getIngredients(recipe).forEach(el => {
-                    if(_.flatText(el).includes(e.target.value)) {
-                        searchResult.push(recipe)
-                        console.log(el)
-                    }
-                })
-                // search on list of ustensils
-                getUstensils(recipe).forEach(el => {
-                    if(_.flatText(el).includes(e.target.value)) {
-                        searchResult.push(recipe)
-                        console.log(el)
-                    }
-                })                  
-                // search on name, description and appliance
-                if(
-                    _.flatText(recipe.name).includes(e.target.value) ||
-                    _.flatText(recipe.description).includes(e.target.value) ||
-                    _.flatText(recipe.appliance).includes(e.target.value)
-                        ) {
-                    searchResult.push(recipe)
-                }  
-            })          
-            new DisplayRecipes(searchResult)
-            console.timeEnd(rand) 
-            if(recipesWrapper.innerHTML.length === 0) {
-                invalidSearch(e.target.value)  
-            }
-        })   
+    if (e.target.value.length < 3) {
+      new DisplayRecipes(recipes)
     }
+    const rand = Math.random().toFixed(4)
+    console.time(rand)
+    recipes.forEach((recipe) => {
+      // search on list of ingredients
+      getIngredients(recipe).forEach((el) => {
+        if (_.flatText(el).includes(e.target.value)) {
+          searchResult.push(recipe)
+        }
+      })
+      // search on list of ustensils
+      getUstensils(recipe).forEach((el) => {
+        if (_.flatText(el).includes(e.target.value)) {
+          searchResult.push(recipe)
+        }
+      })
+      // search on name, description and appliance
+      if (
+        _.flatText(recipe.name).includes(e.target.value) ||
+        _.flatText(recipe.description).includes(e.target.value) ||
+        _.flatText(recipe.appliance).includes(e.target.value)
+      ) {
+        searchResult.push(recipe)
+      }
+    })
+    searchResult = new Set(searchResult)
+    searchResult = Array.from(searchResult)
+    new DisplayRecipes(searchResult)
+    new dropDownMenus(searchResult)
+    console.timeEnd(rand)
+    if (recipesWrapper.innerHTML.length === 0) {
+      invalidSearch(e.target.value)
+    }
+  })
+}
 
-const invalidSearch = value => {
-    hiddenCont.textContent = `votre recherche avec "${value}" ne donne aucun résultat`
+const invalidSearch = (value) => {
+  hiddenCont.textContent = `votre recherche avec "${value}" ne donne aucun résultat`
 }
 /**
  * @param recipe
  * @returns listUstensils of one recipe
  */
-function getUstensils(recipe){
-    let listUstensils = []
-    const {id,name,servings,time,description,appliance,ingredients,...getUstensils} = recipe
-    getUstensils.ustensils.forEach(el => {
-        listUstensils.push(el)   
-    })
-    return listUstensils
+function getUstensils(recipe) {
+  let listUstensils = []
+  const {
+    id,
+    name,
+    servings,
+    time,
+    description,
+    appliance,
+    ingredients,
+    ...getUstensils
+  } = recipe
+  getUstensils.ustensils.forEach((el) => {
+    listUstensils.push(el)
+  })
+  return listUstensils
 }
-
 /**
  * @param recipe
  * @returns listIngredientq of one recipe
  */
-function getIngredients(recipe){
-    let listIngredients = []
-    const {id,name,servings,time,description,appliance,ustensils,...getIngredients} = recipe
-    getIngredients.ingredients.forEach(el => {
-        listIngredients.push(el.ingredient)   
-    })
-        return listIngredients
+function getIngredients(recipe) {
+  let listIngredients = []
+  const {
+    id,
+    name,
+    servings,
+    time,
+    description,
+    appliance,
+    ustensils,
+    ...getIngredients
+  } = recipe
+  getIngredients.ingredients.forEach((el) => {
+    listIngredients.push(el.ingredient)
+  })
+  return listIngredients
 }
 
-
-
+export { search }
