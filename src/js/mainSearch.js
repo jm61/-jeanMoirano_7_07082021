71e50d33f01e5d0a34b6ec838abec3f900274add
jm61 <jean.moirano@gmail.com>
2021-08-09 17:16:50 +0200

    module searchAlgo1

--- src/js/mainSearch.js
+++ src/js/mainSearch.js
@@ -1,6 +1,7 @@
 import { recipes } from "./data.js"
 import { DisplayRecipes } from "./recipes.js"
 import { dropDownMenus } from "./dropDown.js"
+import {searchAlgo1} from './searchAlgo1.js'
 import * as _ from "./utils.js"
 const mainSearch = _.$("#search")
 const hiddenCont = _.$(".search-error-msg")
@@ -35,73 +36,6 @@ export function search() {
 }
 const invalidSearch = (value) => {
   hiddenCont.textContent = `votre recherche avec "${value}" ne donne aucun résultat`
-}
-/**
- * @param recipe
- * @returns listUstensils of one recipe
- */
-function getUstensils(recipe) {
-  let listUstensils = []
-  const {
-    id,
-    name,
-    servings,
-    time,
-    description,
-    appliance,
-    ingredients,
-    ...getUstensils
-  } = recipe
-  getUstensils.ustensils.forEach((el) => {
-    listUstensils.push(el)
-  })
-  return listUstensils
-}
-/**
- * @param recipe
- * @returns listIngredientq of one recipe
- */
-function getIngredients(recipe) {
-  let listIngredients = []
-  const {
-    id,
-    name,
-    servings,
-    time,
-    description,
-    appliance,
-    ustensils,
-    ...getIngredients
-  } = recipe
-  getIngredients.ingredients.forEach((el) => {
-    listIngredients.push(el.ingredient)
-  })
-  return listIngredients
-}
-
-function searchAlgo1(recipes,event) {
-  recipes.forEach((recipe) => {
-    // search on list of ingredients
-    getIngredients(recipe).forEach((el) => {
-      if (_.flatText(el).includes(event.target.value)) {
-        searchResult.push(recipe)
-      }
-    })
-    // search on list of ustensils
-    getUstensils(recipe).forEach((el) => {
-      if (_.flatText(el).includes(event.target.value)) {
-        searchResult.push(recipe)
-      }
-    })
-    // search on name, description and appliance
-    if (
-      _.flatText(recipe.name).includes(event.target.value) ||
-      _.flatText(recipe.description).includes(event.target.value) ||
-      _.flatText(recipe.appliance).includes(event.target.value)
-    ) {
-      searchResult.push(recipe)
-    }
-  })
 }
 
 function refreshDOM(searchResult) {
---
+++ src/js/searchAlgo1.js
@@ -0,0 +1,70 @@
+import * as _ from "./utils.js"
+import {searchResult} from './mainSearch.js'
+
+export function searchAlgo1(recipes,event) {
+    recipes.forEach((recipe) => {
+      // search on list of ingredients
+      getIngredients(recipe).forEach((el) => {
+        if (_.flatText(el).includes(event.target.value)) {
+          searchResult.push(recipe)
+        }
+      })
+      // search on list of ustensils
+      getUstensils(recipe).forEach((el) => {
+        if (_.flatText(el).includes(event.target.value)) {
+          searchResult.push(recipe)
+        }
+      })
+      // search on name, description and appliance
+      if (
+        _.flatText(recipe.name).includes(event.target.value) ||
+        _.flatText(recipe.description).includes(event.target.value) ||
+        _.flatText(recipe.appliance).includes(event.target.value)
+      ) {
+        searchResult.push(recipe)
+      }
+    })
+  }
+
+/**
+ * @param recipe
+ * @returns listUstensils of one recipe
+ */
+ function getUstensils(recipe) {
+    let listUstensils = []
+    const {
+      id,
+      name,
+      servings,
+      time,
+      description,
+      appliance,
+      ingredients,
+      ...getUstensils
+    } = recipe
+    getUstensils.ustensils.forEach((el) => {
+      listUstensils.push(el)
+    })
+    return listUstensils
+  }
+  /**
+   * @param recipe
+   * @returns listIngredientq of one recipe
+   */
+  function getIngredients(recipe) {
+    let listIngredients = []
+    const {
+      id,
+      name,
+      servings,
+      time,
+      description,
+      appliance,
+      ustensils,
+      ...getIngredients
+    } = recipe
+    getIngredients.ingredients.forEach((el) => {
+      listIngredients.push(el.ingredient)
+    })
+    return listIngredients
+  }
